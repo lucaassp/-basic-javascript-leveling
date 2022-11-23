42ee5d80f0067c0f005ed9b96c4623bff4170bcf
lucaassp <lucaspereira.student@gmail.com>
2022-11-23 16:09:23 -0300

    alert - confirm - prompt

---
+++ alert - confirm - prompt/index.js
@@ -0,0 +1,7 @@
+const message = window.alert('Message'); // Window object function Alert message
+const confirmation = window.confirm('do you wish to continue?'); // Window object function that returns True or False value
+const inputInfo = window.prompt('Insert name'); // A window object function with input
+
+console.log(typeof(message));
+console.log(typeof(confirmation));
+console.log(typeof(inputInfo));
