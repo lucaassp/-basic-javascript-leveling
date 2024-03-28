6942767f13663e4b38dba689840489fa6f5d7382
lucaassp <lucaspereira.student@gmail.com>
2024-03-28 11:42:41 -0300

    Loops - Control_Structure

---
+++ control_structure/index.js
@@ -0,0 +1,12 @@
+let number = prompt('Enter a number: ')
+
+if (number > Math.PI) {
+  console.log('The number entered is greater than PI')
+} else if (number < Math.PI) {
+  console.log('The number entered is less than PI')
+} else if ( number == Math.PI) {
+  console.log('The number entered is equal to PI')
+}
+
+console.log(Math.PI)
+
