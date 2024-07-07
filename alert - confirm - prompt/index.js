e7159d47a9f9389753c029960d4336010811e024
lucaassp <lucaspereira.student@gmail.com>
2024-07-07 19:36:45 -0300

    Alert - confirm - prompt review

--- alert - confirm - prompt/index.js
+++ alert - confirm - prompt/index.js
@@ -1,7 +1,15 @@
-const message = window.alert('Message'); // Window object function Alert message
-const confirmation = window.confirm('do you wish to continue?'); // Window object function that returns True or False value
-const inputInfo = window.prompt('Insert name'); // A window object function with input
+//const message = window.alert('Message'); // Window object function Alert message
+//const confirmation = window.confirm('do you wish to continue?'); // Window object function that returns True or False value
+//const inputInfo = window.prompt('Insert name'); // A window object function with input
 
-console.log(typeof(message));
-console.log(typeof(confirmation));
-console.log(typeof(inputInfo));
+//console.log(typeof(message));
+//console.log(typeof(confirmation));
+//console.log(typeof(inputInfo));
+
+// Exe
+
+const numberOne = window.prompt('Insert number 1');
+const numberTwo = window.prompt('Insert number 2');
+
+sum = parseInt(numberOne) + parseInt(numberTwo);
+console.log(sum);
--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,12 @@
-let name = prompt('What is your name?');
-console.log(name);
+console.log('Initial program!')
+
+let number = prompt('Insert number: ')
+
+function multiplicationTable(num) {
+  for (i = 0; i <=10; i++) {
+    console.log(num + " x " + i + ": " + (num * i))
+  }
+}
+
+multiplicationTable(number)
+
